b7201ef147dafcd51b7c976cb7b127df85ea8108
Akshay Tiwari <tiwariaksh25@gmail.com>
2026-02-12 14:08:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from textblob import TextBlob
+from wordcloud import WordCloud
 
 st.title("WhatsApp Chat Analytics Dashboard")
-
 st.write("Welcome to the dashboard 🚀")
-
-
-st.title("WhatsApp Chat Analyzer (Minimal Test)")
 
 uploaded_file = st.file_uploader("Upload WhatsApp Chat (.txt)", type="txt")
 
